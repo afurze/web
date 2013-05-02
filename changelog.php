768beff802b6cab93cb127f9abe8e8d3f76ebeb8
Andrew Furze <afurze@mymail.mines.edu>
2013-05-02 14:11:08 -0600

    got git change log working

--- changelog.php
+++ changelog.php
@@ -2,20 +2,28 @@
 
 <h2>Changelog</h2>
 
-<?php 
-	// construct curl request
-	$curl = curl_init("https://api.github.com/repos/afurze/web/commits");
-	curl_setopt($curl, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
-	curl_exec($curl);
+<div class="gitlog">
+	<table>
+		<?php 
+			// construct curl request
+			$curl = curl_init("https://api.github.com/repos/afurze/web/commits");
+			curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/4.0");
+			curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+			$info = curl_exec($curl);
 
-	// error check and result print
-	if (!curl_errno($curl)) {
-		$info = curl_getinfo($curl);
+			$info = json_decode($info, true);
 
-		for ($i = 0; $i < 5; $i++) {
-			print $info[i]["sha"];
-		}
-	}
-?>
+			// error check and result print
+			if (!curl_errno($curl)) {
+				for ($i = 0; $i < 5; $i++) {
+					print '<tr class="sha"><td class="label">SHA</td><td class="value">{$info[$i]["sha"]}</td></tr>';
+					print '<tr class="data"><td class="label">Author</td><td class="value">{$info[$i]["commit"]["author"]["name"]}</td></tr>';
+					print '<tr class="data"><td class="label">Date</td><td class="value">{$info[$i]["commit"]["author"]["date"]}</td></tr>';
+					print '<tr class="data"><td class="label">Message</td><td class="value">{$info[$i]["commit"]["message"]}</td></tr>';
+				}		
+			}
+		?>
+	</table>
+</div>
 
 <?php include 'footer.php' ?>
