157f2f94a4890357bfa383e0e4a1dbbccbd12b9b
Andrew Furze <afurze@mymail.mines.edu>
2013-05-02 12:01:41 -0600

    added index content and some styling

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php include 'header.php'; ?>
-	<p>Under Construction</p>
+	<h2>Welcome!</h2>
+	<!--[if IE]><p>This site is not tested in Internet Exploder,
+	you should consider switching to a modern browser such as
+	<a href="firefox.com">Firefox</a> or <a href="chrome.com">Chrome</a>.</p><[endif]-->
+
+	<p>This is the personal site of Andrew Furze<p>
 <?php include 'footer.php'; ?>
