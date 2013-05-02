b748d680bae8ce0fdd1ee2f035f7dbf79ff57089
Andrew Furze <afurze@mymail.mines.edu>
2013-05-02 12:51:29 -0600

    added links and all social media icons

--- contact.php
+++ contact.php
@@ -3,10 +3,10 @@
 <h2>Contact</h2>
 
 <ul class="nolist">
-	<li><span class="facebook"><a href="index.php"></a><span></li>
-	<li><span class="twitter"><a href=""></a></span></li>
-	<li><span class="linkedin"><a href=""></a></span></li>
-	<li><span class="googleplus"><a href=""></a></span></li>
+	<li><a href="http://facebook.com/afurze1"><span class="facebook"></span></a></li>
+	<li><a href="http://twitter.com/aspinyshrub"><span class="twitter"></span></a></li>
+	<li><a href="http://linkedin.com/andrewfurze"><span class="linkedin"></span></a></li>
+	<li><a href="http://plus.google.com/113452222489443574607"><span class="googleplus"></span></a></li>
 </ul>
 
 <?php include 'footer.php' ?>
