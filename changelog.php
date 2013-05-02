dc0d800bd2e1dc281b713470826b616e396a9b50
Andrew Furze <afurze@mymail.mines.edu>
2013-05-02 13:35:40 -0600

    trying to control printing

--- changelog.php
+++ changelog.php
@@ -3,14 +3,18 @@
 <h2>Changelog</h2>
 
 <?php 
+	// construct curl request
 	$curl = curl_init("https://api.github.com/repos/afurze/web/commits");
 	curl_setopt($curl, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
 	curl_exec($curl);
 
+	// error check and result print
 	if (!curl_errno($curl)) {
 		$info = curl_getinfo($curl);
 
-		print $info;
+		for ($i = 0; $i < 5; $i++) {
+			print $info[i]["sha"];
+		}
 	}
 ?>
 
