89f7380b20af80e453fa24a2c354d07f7397de77
Andrew Furze <afurze@mymail.mines.edu>
2013-05-02 12:36:18 -0600

    fix image problems add test link

--- contact.php
+++ contact.php
@@ -3,7 +3,7 @@
 <h2>Contact</h2>
 
 <ul class="nolist">
-	<li><span class="facebook"><a href=""></a><span></li>
+	<li><span class="facebook"><a href="index.php"></a><span></li>
 	<li><span class="twitter"><a href=""></a></span></li>
 	<li><span class="linkedin"><a href=""></a></span></li>
 	<li><span class="googleplus"><a href=""></a></span></li>
