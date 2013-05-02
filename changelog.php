ad26e8a90c5163e17d270630435e9d489f982aa0
Andrew Furze <afurze@mymail.mines.edu>
2013-05-02 13:32:33 -0600

    added user agent to curl request

--- changelog.php
+++ changelog.php
@@ -4,9 +4,10 @@
 
 <?php 
 	$curl = curl_init("https://api.github.com/repos/afurze/web/commits");
+	curl_setopt($curl, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
 	curl_exec($curl);
 
-	if (~curl_errno($curl)) {
+	if (!curl_errno($curl)) {
 		$info = curl_getinfo($curl);
 
 		print $info;
