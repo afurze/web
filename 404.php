75436e4a35e59a9329370beae7b4b300aff05502
Andrew Furze <furze.andrew@gmail.com>
2013-05-04 12:57:50 -0600

    added 404 page

---
+++ 404.php
@@ -0,0 +1,9 @@
+<?php include('header.php'); ?>
+
+<h2>Danger Will Robinson, Danger!</h2>
+
+<p>You have attempted to access a page that does not exist.</p>
+
+<p>Why don't you stick to using the navigation links above.</p>
+
+<?php include('footer.php'); ?>
--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 
 	<div class="attribution">
 		<span class="copyright">&copy; Andrew Furze 2013</span>
-		<span class="updated">Last Update: 5/2/13</span>
+		<span class="updated">Last Update: 5/4/13</span>
 	</div>
 </body>
 </html>
