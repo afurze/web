de6266e64ecf19663b7c0896cfa4d599bcde9ddf
Andrew Furze <furze.andrew@gmail.com>
2013-05-21 22:02:51 -0600

    added some brief project notes

--- projects.php
+++ projects.php
@@ -3,8 +3,12 @@
 <p>This is a page with links to some of my projects,
 	both active and inactive.</p>
 
-<ul>
-	<li><a href="/talkivi/assets/www/list.html">TalKivi</a></li>
-</ul>
+<p>
+	<ul>
+		<li><a href="/talkivi/assets/www/list.html">TalKivi</a>: this is a mobile application 
+			written in the PhoneGap cross-platform development enviornment.  It is best viewed 
+			with some kind of in-browser emulator.</li>
+	</ul>
+</p>
 
 <?php include 'footer.php' ?>
