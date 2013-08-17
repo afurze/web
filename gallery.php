c8257f19dd9f83e531d4b5b705713f2163089f67
Andrew Furze <afurze@mymail.mines.edu>
2013-08-16 20:13:27 -0600

    change nav link to gallery.php and add links to gallery.php

---
+++ gallery.php
@@ -0,0 +1,21 @@
+<?php include 'header.php' ?>
+
+<h2>Gallery</h2>
+
+<p>
+	The web gallery is available <a href="/gallery/">here</a>.
+</p>
+
+<h3>Zip Downloads</h3>
+
+<p>
+	These are the full size and resolution images that can be seen (scaled down) in the web gallery.  
+	Each one is several gigabytes in size, so only download these if you REALLY want the originals.
+</p>
+
+<ul>
+	<li><a href="Europe.zip">Europe</a></li>
+	<li><a href="Furze_Wedding.zip"></a></li>
+</ul>
+
+<?php include 'footer.php' ?>
