10f955f996283d346d33786d7720fb9e9cc0ef94
afurze <furze.andrew@gmail.com>
2014-02-18 16:18:28 -0700

    Move connection checking
    Moved connection error checking to connect.php files

--- processNewClient.php
+++ processNewClient.php
@@ -1,35 +1,37 @@
 <?php
+	include 'header.php';
 	include 'db_write_connect.php';
-	include 'PasswordHash.php';
 
-	// check db
-	if (!mysqli_connect_errno()) {
-		// get vars
-		$firstName = $_POST["firstName"];
-		$lastName = $_POST["lastName"];
-		$homePhone = $_POST["homePhone"];
-		$cellPhone = $_POST["cellPhone"];
-		$workPhone = $_POST["workPhone"];
-		$address = $_POST["address"];
-		$city = $_POST["city"];
-		$state = $_POST["state"];
-		$zip = $_POST["zip"];
-		$email = $_POST["email"];
+	// get vars
+	$firstName = $_POST["firstName"];
+	$lastName = $_POST["lastName"];
+	$homePhone = $_POST["homePhone"];
+	$cellPhone = $_POST["cellPhone"];
+	$workPhone = $_POST["workPhone"];
+	$address = $_POST["address"];
+	$city = $_POST["city"];
+	$state = $_POST["state"];
+	$zip = $_POST["zip"];
+	$email = $_POST["email"];
 
-		//TODO: sanitize
+	//TODO: sanitize
 
-		//TODO: check for existing user
+	//TODO: error checking
 
-		// register new user
-		mysqli_query($db, "INSERT INTO clients (firstName, lastName, homePhone, cellPhone, 
-			workPhone, address, email, city, state, zip) VALUES ('".$firstName."', '".$lastName
-			."', '".$homePhone."', '".$cellPhone."', '".$workPhone."', '".$address."', '".
-			$email."', '".$city."', '".$state."', '".$zip."')"
-		);
+	//TODO: check for existing user
+	$existingUser = mysqli_query($db, "SELECT * FROM clients WHERE ")
 
-		/**echo "INSERT INTO clients (firstName, lastName, homePhone, cellPhone, 
-			workPhone, address, email, city, state, zip) VALUES ('".$firstName."', '".$lastName
-			."', '".$homePhone."', '".$cellPhone."', '".$workPhone."', '".$address."', '".
-			$email."', '".$city."', '".$state."', '".$zip."')";**/
-	}
+	// register new user
+	mysqli_query($db, "INSERT INTO clients (firstName, lastName, homePhone, cellPhone, 
+		workPhone, address, email, city, state, zip) VALUES ('".$firstName."', '".$lastName
+		."', '".$homePhone."', '".$cellPhone."', '".$workPhone."', '".$address."', '".
+		$email."', '".$city."', '".$state."', '".$zip."')"
+	);
+
+	/**echo "INSERT INTO clients (firstName, lastName, homePhone, cellPhone, 
+		workPhone, address, email, city, state, zip) VALUES ('".$firstName."', '".$lastName
+		."', '".$homePhone."', '".$cellPhone."', '".$workPhone."', '".$address."', '".
+		$email."', '".$city."', '".$state."', '".$zip."')";**/
+	
+	include 'footer.php';
 ?>
