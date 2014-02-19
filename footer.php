67856ddba1088afe0e6164a597969a1ca622d1d0
afurze <furze.andrew@gmail.com>
2014-02-18 22:36:16 -0700

    Updated copyright and date modified

--- footer.php
+++ footer.php
@@ -1,8 +1,8 @@
 	</div>
 
 	<div class="attribution">
-		<span class="copyright">&copy; Andrew Furze 2013</span>
-		<span class="updated">Last Update: 8/20/13</span>
+		<span class="copyright">&copy; Andrew Furze 2013-2014</span>
+		<span class="updated">Last Update: 2/18/2014</span>
 	</div>
 </body>
 </html>
