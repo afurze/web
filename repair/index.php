282b049bd0d61b0a7185681ecc576c5aaef61391
afurze <furze.andrew@gmail.com>
2014-02-19 10:43:58 -0700

    Add links to tasks on index

--- repair/index.php
+++ repair/index.php
@@ -1,4 +1,31 @@
 <?php include 'restricted.php' ?>
 <?php include 'restrictedHeader.php' ?>
+<?php include 'db_read_connect.php' ?>
 
+<div id="repairnav" class="repairnav">
+	<?php
+		// create navigation
+		printf("<h2>Welcome ".$_SESSION["username"]."</h2>");
+		printf("<ul>");
+		if($_SESSION["isAdmin"] == true) {
+			printf("<li><a href='addclient.php'>Add Client</a></li>");
+			printf("<li><a href='lookupclient.php'>Lookup Client</a></li>");
+			printf("<li><a href='lookupticket.php</a>Lookup Ticket</a></li>");
+		} else {
+			printf("<li><a href='mytickets.php'>My Tickets</a></li>");
+		}
+		printf("</ul>");
+
+		// list appropriate tickets
+		print("<ul>");
+		if($_SESSION["isAdmin"]) == true {
+			// TODO: all open tickets
+			// TODO: list all open tickets
+		} else {
+			// TODO: get client tickets
+			// TODO: list client tickets
+		}
+	?>
+</div>
+<?php include 'db_close.php' ?>
 <?php include '../footer.php' ?>
