17c600af9af32dbb342d20893af963bce61a8b34
afurze <furze.andrew@gmail.com>
2014-02-20 17:11:48 -0700

    Create client lookup form

--- repair/lookupclient.php
+++ repair/lookupclient.php
@@ -1,6 +1,13 @@
 <?php include 'restricted.php' ?>
 <?php include 'restrictedHeader.php' ?>
 
-TODO
+<form action="processclientlookup.php" mehtod="post">
+	<dl>
+		<dt>Client ID:</dt><dd><input type="number" size="11" maxlength="11"></dd>
+		<dt>First Name:</dt><dd><input type="text"></dd>
+		<dt>Last Name:</dt><dd><input type="text>"></dd>
+		<dt>Phone Number:</dt><dd><input type="number" size="10" pattern=".{10}"></dd>
+	</dl>
+</form>
 
-<?php include 'footer.php' ?>
+<?php include '../footer.php' ?>
