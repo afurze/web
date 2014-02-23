f9c246cb9284b11746fdc6913a89a7f3440b69e0
afurze <furze.andrew@gmail.com>
2014-02-22 22:00:47 -0700

    finally fixed stupid logic bug

--- repair/processclientlookup.php
+++ repair/processclientlookup.php
@@ -67,10 +67,12 @@
 		foreach ($rows as $row) {
 			foreach ($row as $key => $value) {
 				if (!(gettype($key) === "integer")) { // check for duplicate pairs
-					if ($value != "") { // for non-empty values
-						printf("<dt>" . $key . "</dt><dd>" . $value . "</dd>");
-					} elseif ($value == "0" || $value === "") { // check for bad values
+					if ($value === "0") { // for non-empty values
+						printf("<dt>" . $key . "</dt><dd style='clear:both;'></dd>");
+					} elseif ($value === "") { // check for bad values
 						printf("<dt>" . $key . "</dt><dd style='clear:both'></dd>");
+					} else {
+						printf("<dt>" . $key . "</dt><dd>" . $value . "</dd>");
 					}
 				}
 			}
