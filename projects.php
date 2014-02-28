7020d71fe3cde14cca9f0ece93da5d01ae11a494
afurze <furze.andrew@gmail.com>
2014-02-27 19:13:10 -0700

    Added java project

--- projects.php
+++ projects.php
@@ -9,6 +9,10 @@
 			Currently working on writing a client management system (using php and MySQL),
 			for what may or may not actually turn into a small repair gig.
 		</li>
+		<li>
+			Working on a Java project to learn about encryption methods as well as working
+			on learning Java more completely.  Hopefully this will get me closer to the 
+			comfort level I have with C++.
 	</ul>
 </p>
 
