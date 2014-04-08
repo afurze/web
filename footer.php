accfbb6b56b21afcb4f3e8132ebce01ff3f356a1
afurze <furze.andrew@gmail.com>
2014-04-07 21:55:33 -0600

    change updated date

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 
 	<div class="attribution">
 		<span class="copyright">&copy; Andrew Furze 2013-2014</span>
-		<span class="updated">Last Update: 3/31/2014</span>
+		<span class="updated">Last Update: 4/7/2014</span>
 	</div>
 </body>
 </html>
